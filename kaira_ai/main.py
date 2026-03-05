44054d8fc14f5084390353f7de654cb0162da842
Madhu <madhu@example.com>
2026-03-05 12:11:03 +0530

    Fix Import Error for Render deployment

--- kaira_ai/main.py
+++ kaira_ai/main.py
@@ -1,4 +1,7 @@
 import os
+import sys
+# Add current directory to sys.path for absolute imports
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import json
 import logging
 import numpy as np
